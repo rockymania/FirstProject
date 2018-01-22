0bb25ad6fe699d7d2d794dc52b9aea22c1bc960d
Ricky <ak47ricky24@gmail.com>
2018-01-22 16:30:45 +0800

    辦帳號

---
+++ View/SignUp.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SignUp.aspx.cs" Inherits="View_SignUp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+
+    <style>
+       #Panel1
+        {
+            position:absolute;
+            top:5%;
+            left:40%;
+        }
+        
+        #SubmitButton 
+        {
+            position:absolute;
+            
+        }
+
+        #ClearButton 
+        {
+            position:absolute;
+            left:55%;
+        }
+
+    </style>
+
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <asp:Panel ID="Panel1" runat="server" Height="309px" Width="221px">
+            <asp:Label ID="AccountTitle" runat="server" Text="帳號"></asp:Label>
+            <br />
+            <asp:TextBox ID="AccountText" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="PasswordTitle" runat="server" Text="密碼"></asp:Label>
+            <br />
+            <asp:TextBox ID="PasswordText" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="AgianPasswordTitle" runat="server" Text="再次輸入密碼"></asp:Label>
+            <br />
+            <asp:TextBox ID="AgianPasswordText" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="MailTitle" runat="server" Text="信箱"></asp:Label>
+            <br />
+            <asp:TextBox ID="MailText" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="SubmitButton" runat="server" Text="確認" OnClick="SubmitButton_Click" />
+            <asp:Button ID="ClearButton" runat="server" Text="清除" OnClick="ClearButton_Click" />
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ View/SignUp.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class View_SignUp : System.Web.UI.Page
+{
+    private string mServerUrl = "http://192.168.8.104/";
+    private string mURL = "CreateAccount.aspx?Account={0}&Password={1}&Email={2}";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ClearButton_Click(object sender, EventArgs e)
+    {
+        AccountText.Text = "";
+        PasswordText.Text = "";
+        AgianPasswordText.Text = "";
+        MailText.Text = "";
+    }
+
+    protected void SubmitButton_Click(object sender, EventArgs e)
+    {
+        if (CheckAccount() == false)
+            return;
+
+        if (CheckPassword() == false)
+            return;
+
+        if (CheckMail() == false)
+            return;
+
+        string zURL = mServerUrl + string.Format(mURL, AccountText.Text, PasswordText.Text, MailText.Text);
+
+        string vResult = string.Empty;
+
+        using (WebClient vWb = new WebClient())
+        {
+            vResult = vWb.DownloadString(zURL);
+        }
+
+        //Response.Write(vResult);
+
+
+        GetResultString(vResult);
+    }
+
+    private bool CheckAccount()
+    {
+        if (AccountText.Text == "")
+        {
+            Response.Write("帳號為空");
+            return false ;
+        }
+
+        return true;
+    }
+
+    private bool CheckPassword()
+    {
+        if (PasswordText.Text != AgianPasswordText.Text)
+        {
+            Response.Write("密碼不符");
+            return false;
+        }
+
+        if (PasswordText.Text == "")
+        {
+            Response.Write("密碼為空");
+            return false;
+        }
+
+        if (AgianPasswordText.Text == "")
+        {
+            Response.Write("再次輸入密碼為空");
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool CheckMail()
+    {
+        if (MailText.Text == "")
+            return false;
+
+        int vIndex = -1;
+
+        vIndex = MailText.Text.IndexOf('@');
+
+        if (vIndex == -1)
+            return false;
+
+        return true;
+    }
+
+    private string GetResultString(string vResult)
+    {
+        string zResultString = string.Empty;
+
+        switch (vResult)
+        {
+            case "0":
+                zResultString = "成功";
+                break;
+            case "1":
+                zResultString = "帳號重複";
+                break;
+            case "2":
+                zResultString = "信箱重複";
+                break;
+        }
+
+        return zResultString;
+    }
+}
